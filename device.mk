6c9d7cf9fe4d978fc035c65fa51a254fb79daefd
Ryan Andri <ryan-andri@outlook.com>
2020-03-21 16:32:18 +0700

    tulip: overlay: adapt to CarbonROM

--- device.mk
+++ device.mk
@@ -33,12 +33,11 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-carbon
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(DEVICE_PATH)/overlay-lineage/lineage-sdk \
-    $(DEVICE_PATH)/overlay-lineage/packages/apps/Snap
+    $(DEVICE_PATH)/overlay/packages/apps/Snap
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
