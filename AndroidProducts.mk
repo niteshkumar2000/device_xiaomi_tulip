9e0cf3ca92ed2cfb7ddb4dc2dcc9d5373547e23a
Niteshkumar <nitesh156200@gmail.com>
2020-04-19 10:15:17 +0530

    tulip: bringup syberia!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tulip.mk
+    $(LOCAL_DIR)/syberia_tulip.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_tulip-user \
-    lineage_tulip-userdebug \
-    lineage_tulip-eng
+    syberia_tulip-user \
+    syberia_tulip-userdebug \
+    syberia_tulip-eng
